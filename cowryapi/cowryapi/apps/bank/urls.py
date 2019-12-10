70f6f886824e8a15ce5e8d7d9af1b5074fd70c78
fejiroofficial <fejiro.gospel@gmail.com>
2019-12-10 22:06:07 +0100

    Removing test files

--- cowryapi/cowryapi/apps/bank/urls.py
+++ cowryapi/cowryapi/apps/bank/urls.py
@@ -2,16 +2,15 @@ from django.urls import path
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from .views import (BankView, BankCustomerJoinView,
-                    ApproveCustomerView, PaymentView)
+                    ApproveRetrieveCustomerView, PaymentView)
 
 
 router = DefaultRouter()
 router.register(r'banks', BankView, basename='banks')
-# router.register()
 
 urlpatterns = [
     path('banks/<int:bank_id>/join/', BankCustomerJoinView.as_view()),
-    path('banks/<int:bank_id>/customers/<int:pk>/', ApproveCustomerView.as_view()),
+    path('banks/<int:bank_id>/customers/<int:pk>/', ApproveRetrieveCustomerView.as_view()),
     path('banks/<int:bank_id>/customers/<int:pk>/payment/', PaymentView.as_view())
 ]
 
